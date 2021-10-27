2b51a47c7037debcba5249688a176e43e5dba0bb
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-10-27 13:01:30 -0300

    Remove express-session, multer gridfs-stream and GridFsStorage

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express';
-import session from 'express-session';
-import flash from 'connect-flash';
+import express from 'express';
 
 const app = express();
-
-// Express session
-  app.use(session({
-    secret: process.env.SECRET_KEY,
-    resave: true,
-    saveUninitialized: true
-  }));
-  app.use(flash());
-
-// Middleware
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    next();
-  });
 
 // Express config
   app.use(express.json());
