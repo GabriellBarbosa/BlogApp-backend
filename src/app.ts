ff2ed6d4184a3fdbb8144e6762aa5e5e487e40b4
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-10-29 20:58:16 -0300

    indentation adjustment

--- src/app.ts
+++ src/app.ts
@@ -11,24 +11,24 @@ createConnection();
 const app = express();
 
 // @desc Express config
-  app.use(express.json());
+app.use(express.json());
 
 // @desc Routes
-  app.use('/admin', admin);
-  app.use('/posts', post);
+app.use('/admin', admin);
+app.use('/posts', post);
 
 // @desc Handle app errors
-  app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
-    if(err instanceof AppError) {
-      return res.status(err.statusCode).json({
-        message: err.message
-      });
-    }
-
-    return res.status(500).json({
-      status: 'Error',
-      message: `Internal server error: ${err.message}`
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  if(err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message
     });
+  }
+
+  return res.status(500).json({
+    status: 'Error',
+    message: `Internal server error: ${err.message}`
   });
+});
 
 export { app };
