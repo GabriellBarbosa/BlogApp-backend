50760f10c9f3109fa384e15aeeea80c63659db20
Gabriel Barbosa <bielbarbosa900@gmail.com>
2021-10-29 14:53:52 -0300

    add desc comment

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,6 +4,7 @@ import { PostController } from '@controllers/PostController';
 const routes = Router();
 const postController = new PostController();
 
+// @desc Add a new post
 routes.post('/add', postController.add);
 
 export { routes };
